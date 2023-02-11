f674b0795c3765b90b260fbe10f07ee3e9117fd0
Jordi Adan <jordi.adan@adevinta.com>
2023-02-11 23:14:23 +0100

    Log GITHUB_EVENT_NAME

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const core = require('@actions/core');
 async function run() {
   try {
     const repoName = core.getInput('repo-name');
+
+    console.log(process.env.GITHUB_EVENT_NAME)
+
     const pullRequestTitle = process.env.GITHUB_EVENT_NAME.pull_request.title;
 
     console.log(`Checking pull request title for repository: ${repoName}`);
