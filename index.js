c7e9e1f5cb8346fe88c19e2e44c4695b77bc93cf
Jordi Adan <jordi.adan@adevinta.com>
2023-02-11 23:59:33 +0100

    Refactor github action

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const core = require('@actions/core');
+const { GitHub, context } = require("@actions/github");
 
 async function run() {
-  try {
-    const repoName = core.getInput('repo-name');
+  const { payload, eventName } = context;
 
-    console.log(process.env.GITHUB_EVENT_NAME)
-
-    const pullRequestTitle = process.env.GITHUB_EVENT_NAME.pull_request.title;
-
-    console.log(`Checking pull request title for repository: ${repoName}`);
-
-    if (!pullRequestTitle.match(/^(FEATURE|FIX|TECH|DOCS)/)) {
-      core.setFailed(`The pull request title does not start with FEATURE, FIX, TECH, or DOCS.`);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+  if (eventName !== "pull_request") {
+    console.log("This Action only runs on pull_request events.");
+    return;
   }
+
+  const allowedPrefixes = ["FEATURE", "FIX", "TECH", "DOCS"];
+  const pullRequestTitle = payload.pull_request.title;
+
+  const prefix = pullRequestTitle.split(" ")[0];
+  if (!allowedPrefixes.includes(prefix)) {
+    console.log(
+        `Error: The title must start with one of the following prefixes: ${allowedPrefixes.join(
+            ", "
+        )}`
+    );
+    return;
+  }
+
+  console.log("The pull request title is valid.");
 }
 
 run();
