6fced825781349926474f07426dd27b6d2334ad7
Jordi Adan <jordi.adan@adevinta.com>
2023-02-12 01:11:14 +0100

    Update dist files

--- dist/index.js
+++ dist/index.js
@@ -13,14 +13,19 @@ async function run() {
     return;
   }
 
+  console.log("Starting action...")
   const pullRequest = context.payload.pull_request;
   const title = pullRequest.title;
+  console.log("Title: " + pullRequest.title)
 
   const allowedPrefixes = ["FEATURE", "FIX", "TECH", "DOCS"];
   const allowedRegex = new RegExp(`^(${allowedPrefixes.join("|")}):`);
 
   if (!allowedRegex.test(title)) {
+    console.log("Error: The title must start with one of the following prefixes: FEATURE, FIX, TECH, DOCS");
     setFailed("Error: The title must start with one of the following prefixes: FEATURE, FIX, TECH, DOCS");
+  } else {
+    console.log("The pull request title is valid.");
   }
 }
 
