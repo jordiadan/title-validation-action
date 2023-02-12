a148a3901375c46b18fa15eddeff9ed575c9e143
Jordi Adan <jordi.adan@adevinta.com>
2023-02-12 01:32:20 +0100

    Update dist files

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const { setFailed } = __nccwpck_require__(2186);
-const { GitHub, context } = __nccwpck_require__(5438);
+const { context } = __nccwpck_require__(5438);
 
 async function run() {
 
@@ -15,20 +15,20 @@ async function run() {
     setFailed("This Action only runs on pull_request events.");
   }
 
-  console.log("Starting action...")
   const pullRequestTitle = payload.pull_request.title;
-  console.log("Title: " + pullRequestTitle.title)
 
   const allowedPrefixes = ["FEATURE", "FIX", "TECH", "DOCS"];
   const allowedRegex = new RegExp(`^(${allowedPrefixes.join("|")}):`);
 
+
   if (!allowedRegex.test(pullRequestTitle)) {
-    console.log("Error: The title must start with one of the following prefixes: FEATURE, FIX, TECH, DOCS");
     setFailed("Error: The title must start with one of the following prefixes: FEATURE, FIX, TECH, DOCS");
   } else {
     console.log("The pull request title is valid.");
   }
 }
+
+run();
 
 module.exports = run;
 
